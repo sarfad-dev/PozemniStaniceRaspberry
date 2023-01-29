cefd164305dbb76a00e9393283db1255db1a14f4
Mors <119057484+MorssEXE@users.noreply.github.com>
2023-01-29 21:34:18 +0100

    k nicemu

--- test.py
+++
@@ -1,9 +0,0 @@
-import serial
-ser = serial.Serial('/dev/ttyACM0', 115200)
-read_line = ser.readline().decode("utf-8").strip('\n').strip('\r')
-if read_line > 1000000:
-    print("pressure")
-elif (read_line[-2:]==24):
-    print("temperature")
-elif (read_line[-2:]==32 and read_line<100000):
-    print("humidity")   
