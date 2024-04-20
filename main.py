62c6d104d052181d457dbb46f54aaa85a352f015
Daniel Tomis <119057484+MorssEXE@users.noreply.github.com>
2024-04-21 00:47:20 +0200

    Update main.py
    made the code a lot different and also added insertion to mysql db

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
-import serial,json,csv,datetime
+import serial
+import os
+from datetime import datetime
+from mysql.connector import Error
+import mysql.connector
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+serial = serial.Serial('/dev/ttyS0', 115200, timeout=1)
 
-data = {}
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
+
+
+try:
+    db = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
+    cursor = db.cursor()
+
+except Error as e:
+    print(f"Error: {e}")
+    exit(1)
 
 while True:
-    read_line = ser.readline().decode("utf-8").strip('\n').strip('\r')
-    if read_line[-3:]== " 16":
-        pressure = float(read_line[:-3]) / 10
-        data["pressure"] = pressure
-    elif read_line[-3:]==" 24":
-        temperature = float(read_line[:-3]) / 10
-        data["temperature"] = temperature
-    elif read_line[-3:]==" 32":
-        humidity = float(read_line[:-3]) / 10
-        data["humidity"] = humidity
-    
-    with open("data.json", "w") as outfile:
-        json.dump(data, outfile)
-        print(data)
-        
-    with open("data.csv", "a") as outfile:
-        writer = csv.writer(outfile)
-        
+    try:
+        data = serial.readline().decode().strip()
+
+        values = data.split(';')
+
+        timestamp = datetime.now()
+        time_str = timestamp.strftime('%H:%M:%S')
+
+        insert_query = "INSERT INTO table_data (timestamp, time, temperature, pressure, humidity, latitude, longitude, height, velocity) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        data_tuple = (timestamp, time_str, *values)
+        cursor.execute(insert_query, data_tuple)
+        db.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+
+cursor.close()
+db.close()
