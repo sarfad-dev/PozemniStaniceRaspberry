09a8591e2f0f47ae59441848659600e50667e8f8
Daniel Tomis <119057484+MorssEXE@users.noreply.github.com>
2024-05-05 19:33:32 +0200

    feat: insterting into db based off of time from serial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from mysql.connector import Error
 import mysql.connector
 from dotenv import load_dotenv
@@ -8,7 +8,6 @@ import serial.tools.list_ports
 from colorama import init, Fore, Style
 
 init()
-
 load_dotenv()
 
 db_host = os.getenv('DB_HOST')
@@ -28,7 +27,6 @@ if port_number < 0 or port_number >= len(ports):
     exit(1)
 
 port_device = ports[port_number].device
-arduino_serial = serial.Serial(port_device, 115200, timeout=1)
 
 try:
     db = mysql.connector.connect(
@@ -42,18 +40,33 @@ except Error as e:
     print(f"{Fore.RED}Error: {e}{Style.RESET_ALL}")
     exit(1)
 
+arduino_serial = serial.Serial(port_device, 9600)
+
+last_inserted_second = None
+
 while True:
     try:
         data = arduino_serial.readline().decode().strip()
+
         values = data.split(';')
-        timestamp = datetime.now()
-        time_str = timestamp.strftime('%H:%M:%S')
-        insert_query = "INSERT INTO table_data (timestamp, time, temperature, pressure, humidity, latitude, longitude, height, velocity) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        data_tuple = (timestamp, time_str, *values)
-        cursor.execute(insert_query, data_tuple)
-        db.commit()
+
+        serial_time = datetime.strptime(values[0], '%H:%M:%S') + timedelta(hours=2)
+        time_str = serial_time.strftime('%H:%M:%S')
+        current_second = serial_time.second
+
+        if current_second != last_inserted_second:
+            timestamp = datetime.now()
+            insert_query = "INSERT INTO sarfad_data (timestamp, time, temperature, pressure, humidity, latitude, longitude, height, velocity) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            
+            data_tuple = (timestamp, time_str, *values[1:])
+            print(data_tuple)
+
+            cursor.execute(insert_query, data_tuple)
+            db.commit()
+
+            last_inserted_second = current_second
     except Exception as e:
-        print(f"{Fore.RED}Error: {e}{Style.RESET_ALL}")
+        print(f"Error: {e}")
 
 cursor.close()
 db.close()
